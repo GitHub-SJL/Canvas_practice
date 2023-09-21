4614b8c8df008293ed9cac950a25bd492ae122f9
GitLabSJ <aeeou12@gmail.com>
2023-09-21 16:11:54 +0900

    #1-2 파티클 그리기

--- 1. 파티클 기초/index.js
+++ 1. 파티클 기초/index.js
@@ -14,4 +14,29 @@ canvas.width = canvasWidth * dpr;
 canvas.height = canvasHeight * dpr;
 ctx.scale(dpr, dpr);
 
-ctx.fillRect(10, 10, 50, 50);
+class Particle {
+  constructor(x, y, radius) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, (Math.PI / 180) * 360);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+const x = 100;
+const y = 100;
+const radius = 50;
+const particle = new Particle(x, y, radius);
+
+function animate() {
+  window.requestAnimationFrame(animate);
+
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+  particle.draw();
+}
+animate();
