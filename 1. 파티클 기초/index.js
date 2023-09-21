6420d77f53cdcce15261429bda7aa4d85c8551d8
GitLabSJ <aeeou12@gmail.com>
2023-09-21 18:41:17 +0900

    #1-6 dat.GUI 활용하여 테스트

--- 1. 파티클 기초/index.js
+++ 1. 파티클 기초/index.js
@@ -14,6 +14,44 @@ canvas.width = canvasWidth * dpr;
 canvas.height = canvasHeight * dpr;
 ctx.scale(dpr, dpr);
 
+const feGaussianBlur = document.querySelector("feGaussianBlur");
+const fecolorMatrix = document.querySelector("fecolorMatrix");
+
+const controls = new (function () {
+  this.blurValue = 40;
+  this.alphaChannel = 100;
+  this.alphaOffset = -23;
+  this.acc = 1.03;
+})();
+
+let gui = new dat.GUI();
+
+const f1 = gui.addFolder("Gooey Effect");
+f1.open();
+f1.add(controls, "blurValue", 0, 100).onChange((value) => {
+  feGaussianBlur.setAttribute("stdDeviation", value);
+});
+
+f1.add(controls, "alphaChannel", 1, 200).onChange((value) => {
+  fecolorMatrix.setAttribute(
+    "values",
+    `1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 ${value} ${controls.alphaOffset}`
+  );
+});
+
+f1.add(controls, "alphaOffset", -40, 40).onChange((value) => {
+  fecolorMatrix.setAttribute(
+    "valuse",
+    `1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 ${controls.alphaChannel} ${value}`
+  );
+});
+
+const f2 = gui.addFolder("Particle Property");
+f2.open();
+f2.add(controls, "acc", 1, 1.5, 0.01).onChange((value) => {
+  particles.forEach((particle) => (particle.acc = value));
+});
+
 class Particle {
   constructor(x, y, radius, vy) {
     this.x = x;
