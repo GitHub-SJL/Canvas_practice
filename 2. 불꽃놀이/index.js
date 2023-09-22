bc84d1e9f3760d5814c38d2270411ddf784022eb
GitLabSJ <aeeou12@gmail.com>
2023-09-22 17:48:45 +0900

    #2-10 스파크, 파티클 컬러값을 같은 랜덤으로 바꾸기

--- 2. 불꽃놀이/index.js
+++ 2. 불꽃놀이/index.js
@@ -31,11 +31,11 @@ class Canvas extends CanvasOption {
       this.canvasWidth * 0.8
     );
     const vy = this.canvasHeight * randomNumberBewtween(0.01, 0.015) * -1;
-    const color = `255, 255, 255`;
-    this.tails.push(new Tail(x, vy, color));
+    const colorDeg = randomNumberBewtween(0, 360);
+    this.tails.push(new Tail(x, vy, colorDeg));
   }
 
-  createParticles(x, y, color) {
+  createParticles(x, y, colorDeg) {
     const PARTICLE_NUM = 400;
 
     for (let i = 0; i < PARTICLE_NUM; i++) {
@@ -48,8 +48,9 @@ class Canvas extends CanvasOption {
       const vx = r * Math.cos(angle);
       const vy = r * Math.sin(angle);
       const opacity = randomNumberBewtween(0.6, 0.9);
+      const _colorDeg = randomNumberBewtween(-20, 20) + colorDeg;
 
-      this.particles.push(new Particle(x, y, vx, vy, opacity, color));
+      this.particles.push(new Particle(x, y, vx, vy, opacity, _colorDeg));
     }
   }
 
@@ -70,9 +71,18 @@ class Canvas extends CanvasOption {
         tail.update();
         tail.draw();
 
+        for (let i = 0; i < Math.round(-tail.vy * 0.5); i++) {
+          const vx = randomNumberBewtween(-5, 5) * 0.05;
+          const vy = randomNumberBewtween(-5, 5) * 0.05;
+          const opacity = Math.min(-tail.vy, 0.5);
+          this.sparks.push(
+            new Spark(tail.x, tail.y, vx, vy, opacity, tail.colorDeg)
+          );
+        }
+
         if (tail.vy > -0.7) {
           this.tails.splice(index, 1);
-          this.createParticles(tail.x, tail.y, tail.color);
+          this.createParticles(tail.x, tail.y, tail.colorDeg);
         }
       });
 
@@ -81,17 +91,17 @@ class Canvas extends CanvasOption {
         particle.draw();
 
         if (Math.random() < 0.1) {
-          this.sparks.push(new Spark(particle.x, particle.y, 0.3));
+          this.sparks.push(new Spark(particle.x, particle.y, 0, 0, 0.3,45));
         }
 
         if (particle.opacity < 0) this.particles.splice(index, 1);
       });
 
-      this.sparks.forEach((spark, index) => {
-        spark.update();
-        spark.draw();
-        if (spark.opacity < 0) this.sparks.splice(index, 1);
-      });
+      // this.sparks.forEach((spark, index) => {
+      //   spark.update();
+      //   spark.draw();
+      //   if (spark.opacity < 0) this.sparks.splice(index, 1);
+      // });
 
       then = now - (delta % this.interval);
     };
