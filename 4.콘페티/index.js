02979540cb57059b40167d16553d9d5e60f11b61
GitLabSJ <aeeou12@gmail.com>
2023-09-25 12:49:56 +0900

    4-4 특정 각도로 쏘는 콘페티

--- 4.콘페티/index.js
+++ 4.콘페티/index.js
@@ -17,13 +17,11 @@ function init() {
   canvas.width = canvasWidth * dpr;
   canvas.height = canvasHeight * dpr;
   ctx.scale(dpr, dpr);
-
-  confetti({ x: canvasWidth / 2, y: canvasHeight / 2, count: 10 });
 }
 
-function confetti({ x, y, count }) {
+function confetti({ x, y, count, deg }) {
   for (let i = 0; i < count; i++) {
-    particles.push(new Particle(x, y));
+    particles.push(new Particle(x, y, deg));
   }
 }
 
@@ -47,7 +45,9 @@ function render() {
   };
   requestAnimationFrame(frame);
 }
-
+window.addEventListener("click", () => {
+  confetti({ x: 0, y: canvasHeight / 2, count: 10, deg: -50 });
+});
 window.addEventListener("load", () => {
   init();
   render();
